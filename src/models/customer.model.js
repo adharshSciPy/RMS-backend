e97608de52cb5e21727b67253f0610c8ad59992a
adharsh <adharsh@maptell.com>
2024-05-29 15:57:16 +0530

    feature: customer schema created

---
+++ src/models/customer.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from 'mongoose'
+
+const customerSchema = new Schema({
+    name: {
+        type: String,
+    },
+    phoneNumber: {
+        type: Number,
+    },
+    email: {
+        type: Number,
+    },
+    address: {
+        type: Number,
+    },
+    location: {
+        type: Geolocation
+    },
+    transactionIds: {
+        type: [Schema.Types.ObjectId],
+        ref: 'Transaction'
+    }
+},
+    { timestamps: true })
+
+
+export const Menu = mongoose.model("Customer", customerSchema)
